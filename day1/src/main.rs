3351b31644e9ece7d2292dcd6b911486acf3d923
Pan Shaun <Shaun.Pan@fil.com>
2022-12-02 08:38:00 +0800

    tweak

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,10 +3,10 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut result = get_most_calories();
+    let mut result = get_calories();
     result.sort();
 
-    let outcome1: usize = *result.iter().rev().take(1).next().unwrap();
+    let outcome1 = result.iter().rev().next().unwrap();
     let outcome2: usize = result.iter().rev().take(3).sum();
 
     println!("The most Calories: {}", outcome1);
@@ -21,7 +21,7 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn get_most_calories() -> Vec<usize> {
+fn get_calories() -> Vec<usize> {
     let mut temp_value = 0;
     let mut result: Vec<usize> = Vec::new();
     if let Ok(lines) = read_lines("./input.txt") {
