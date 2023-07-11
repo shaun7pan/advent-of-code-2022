199cbe30f6dad8788cf333a855703488755a5198
Pan Shaun <Shaun.Pan@fil.com>
2023-07-11 13:40:59 +0800

    tidy up

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -10,21 +10,12 @@ use nom::sequence::separated_pair;
 use nom::IResult;
 use nom::Parser;
 
-#[derive(Debug, Eq)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum Packet {
     List(Vec<Packet>),
     Number(u32),
 }
-impl PartialEq for Packet {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Packet::List(l), Packet::List(r)) => l == r,
-            (Packet::List(l), Packet::Number(r)) => l == &vec![Packet::Number(*r)],
-            (Packet::Number(l), Packet::List(r)) => &vec![Packet::Number(*l)] == r,
-            (Packet::Number(l), Packet::Number(r)) => l == r,
-        }
-    }
-}
+
 impl PartialOrd for Packet {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
@@ -41,35 +32,10 @@ impl Ord for Packet {
     }
 }
 
-//[1,1,3,1,1]
-//[1,1,5,1,1]
-//
-//[[1],[2,3,4]]
-//[[1],4]
-//
-//[9]
-//[[8,7,6]]
-//
-//[[4,4],4,4]
-//[[4,4],4,4,4]
-//
-//[7,7,7,7]
-//[7,7,7]
-//
-//[]
-//[3]
-//
-//[[[]]]
-//[[]]
-//
-//[1,[2,[3,[4,[5,6,7]]]],8,9]
-//[1,[2,[3,[4,[5,6,0]]]],8,9]
-
 fn packet(input: &str) -> IResult<&str, Packet> {
     alt((
-        delimited(tag("["), separated_list0(tag(","), packet), tag("]"))
-            .map(|vec| Packet::List(vec)),
-        complete::u32.map(|n| Packet::Number(n)),
+        delimited(tag("["), separated_list0(tag(","), packet), tag("]")).map(Packet::List),
+        complete::u32.map(Packet::Number),
     ))(input)
 }
 
@@ -101,7 +67,7 @@ pub fn process_part1(input: &str) -> String {
 }
 
 pub fn process_part2(input: &str) -> String {
-    let (_, mut pairs) = pairs(input).unwrap();
+    let (_, pairs) = pairs(input).unwrap();
     let packet_2 = Packet::List(vec![Packet::List(vec![Packet::Number(2)])]);
     let packet_6 = Packet::List(vec![Packet::List(vec![Packet::Number(6)])]);
 
